fbc94daf9e52a4f3b6154766d3fe91160e969bad
RishikeshSV <rishuvidyarthi@gmail.com>
2021-10-15 17:40:34 +0530

    fixed filter issue

--- public/js/filter.js
+++ public/js/filter.js
@@ -4,66 +4,84 @@ const list = document.querySelectorAll(".restaurant-details");
 const parent = document.querySelector(".restaurant-list");
 let ar = Array.from(list);
 
-//VEG NONVEG FILTER
 const veg = document.getElementById("veg");
+const city = document.querySelector("#city");
+const rating = document.querySelector("#rating");
+
+//VEG NONVEG FILTER
+
 veg.onchange = () => {
-  if ($("#veg").prop("checked")) {
-    for (var i = 0; i < ar.length; ++i) {
-      let x = ar[i].querySelector(".tags-name").innerHTML.split(",");
-      if (x.includes("nonveg")) {
-        document.getElementById(ar[i].id).style.display = "none";
-      }
-    }
-  } else {
-    for (var i = 0; i < ar.length; ++i) {
-      let x = ar[i].querySelector(".tags-name").innerHTML.split(",");
-      if (x.includes("nonveg")) {
-        document.getElementById(ar[i].id).style.display = "flex";
-      }
-    }
-  }
+  filter();
 };
 
 //PLACE FILTER
-const city = document.querySelector("#city");
+
 city.onchange = () => {
-  if (city.value !== "City") {
-    for (var i = 0; i < ar.length; ++i) {
-      if (city.value !== ar[i].querySelector(".place-name").innerHTML) {
-        document.getElementById(ar[i].id).style.display = "none";
-      } else {
-        document.getElementById(ar[i].id).style.display = "flex";
-      }
-    }
-  } else {
-    for (var i = 0; i < ar.length; ++i) {
-      document.getElementById(ar[i].id).style.display = "flex";
-    }
-  }
+  filter();
 };
 
 //RATING FILTER
-const rating = document.querySelector("#rating");
+
 rating.onchange = () => {
-  if (rating.value > 0) {
-    for (var i = 0; i < ar.length; ++i) {
-      if (
-        parseFloat(rating.value) >
-        parseFloat(ar[i].querySelector(".rating-number").innerHTML)
-      ) {
-        document.getElementById(ar[i].id).style.display = "none";
-      } else {
-        document.getElementById(ar[i].id).style.display = "flex";
+  filter();
+};
+
+//filter function
+
+function filter() {
+  let fr = ar;
+  fr = vegFilter(fr);
+  fr = cityFilter(fr);
+  fr = ratingFilter(fr);
+  parent.innerHTML = "";
+  for (var i = 0; i < fr.length; i++) {
+    parent.appendChild(fr[i]);
+  }
+}
+
+function vegFilter(vegArr) {
+  let temp = [];
+  if ($("#veg").prop("checked")) {
+    for (var i = 0; i < vegArr.length; ++i) {
+      let x = vegArr[i].querySelector(".tags-name").innerHTML.split(",");
+      if (!x.includes("nonveg")) {
+        temp.push(vegArr[i]);
       }
     }
   } else {
-    for (var i = 0; i < ar.length; ++i) {
-      document.getElementById(ar[i].id).style.display = "flex";
-    }
+    temp = vegArr;
   }
-};
+  return temp;
+}
 
-//PRICE FILTER
-// $("input[name='price']").onchange = () => {
-//   console.log($("input[name='price']:checked").val());
-// };
+function cityFilter(cityArr) {
+  let temp = [];
+  if (city.value !== "City") {
+    for (var i = 0; i < cityArr.length; ++i) {
+      if (city.value === cityArr[i].querySelector(".place-name").innerHTML) {
+        temp.push(cityArr[i]);
+      }
+    }
+  } else {
+    temp = cityArr;
+  }
+
+  return temp;
+}
+
+function ratingFilter(ratingArr) {
+  let temp = [];
+  if (rating.value > 0) {
+    for (var i = 0; i < ratingArr.length; ++i) {
+      if (
+        parseFloat(rating.value) <=
+        parseFloat(ratingArr[i].querySelector(".rating-number").innerHTML)
+      ) {
+        temp.push(ratingArr[i]);
+      }
+    }
+  } else {
+    temp = ratingArr;
+  }
+  return temp;
+}
